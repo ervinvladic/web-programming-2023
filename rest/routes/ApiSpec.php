bb62eaa2e9e9d1a19141b90d185138e819dc9471
Ervin Vladić <ervin.vladic@stu.ibu.edu.ba>
2023-06-12 21:59:18 +0200

    minor changes

--- rest/routes/ApiSpec.php
+++ rest/routes/ApiSpec.php
@@ -2,10 +2,10 @@
 
 
 /**
- * @OA\Info(title="Radnikba API Specs", version="0.2", @OA\Contact(email="harun.kunovac@stu.ibu.edu.ba", name="Harun Kunovac"))
+ * @OA\Info(title="Radnikba API Specs", version="0.2", @OA\Contact(email="ervin.vladic@stu.ibu.edu.ba", name="Ervin Vladic"))
  * @OA\OpenApi(
- *    @OA\Server(url="http://localhost/Web-Programming-2022-radnik.ba/rest", description="Development Environment" ),
- *    @OA\Server(url="https://jellyfish-app-4yhxx.ondigitalocean.app/rest", description="Production Environment" )
+ *    @OA\Server(url="http://localhost/web-programming-2023/rest", description="Development Environment" ),
+ *    @OA\Server(url="https://lobster-app-ibqe5.ondigitalocean.app/", description="Production Environment" )
  * ),
  * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authorization" )
  */
